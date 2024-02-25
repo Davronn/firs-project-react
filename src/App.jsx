c56aba9bf0bd8498f23a61b7897cb9ee12583a95
Davronn <davronmahamadjonov8@gmai.com>
2024-02-26 01:22:26 +0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,350 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+      <div class="desktop-Hvn">
+        <div class="navigation-X4S">
+          <div class="auto-group-twoq-ujC">
+            <img class="group-190-a4e" src="./src/assets/group-190.png" />
+            <p class="product-FwU">Product</p>
+          </div>
+          <p class="product-customers-pr-xUW">
+            <span class="product-customers-pr-xUW-sub">Product</span>
+            <span class="product-customers-pr-xUW-sub">Customers</span>
+            <span class="product-customers-pr-xUW-sub">Pricing</span>
+            <span class="product-customers-pr-xUW-sub">Resources</span>
+          </p>
+          <div class="button-Ujg">Sign In</div>
+          <div class="button-HBL">Sign Up</div>
+          <img class="frame-49-wWn" src="./src/assets/frame-49.png" />
+        </div>
+        <div class="auto-group-w2op-qcA">
+          <div class="group-1431-ZHG">
+            <div class="left-side-TtS">
+              <div class="text-ksY">
+                <p class="the-design-thinking-WLv">
+                  Work at the speed
+                  <br />
+                  of thought
+                </p>
+                <p class="tools-tutorials-de-wKk">
+                  Tools, tutorials, design and innovation experts, all in one
+                  place! The most intuitive way to imagine your next user
+                  experience.
+                </p>
+              </div>
+              <div class="button-rxW">
+                <div class="button-QDL">Get started</div>
+                <div class="button-fux">
+                  <img
+                    class="triangle-copy-6-zSS"
+                    src="./src/assets/triangle-copy-6.png"
+                  />
+                  <p class="watch-the-video-upJ">Watch the Video</p>
+                </div>
+              </div>
+            </div>
+            <img
+              class="social-media-amico-ELn"
+              src="./src/assets/social-media-amico.png"
+            />
+          </div>
+        </div>
+        <div class="auto-group-tc7g-Vg2">
+          <div class="feature-3Bk">
+            <p class="we-design-tools-to-u-ZA6">
+              Product was Built Specifically
+              <br />
+              for You
+            </p>
+            <div class="auto-group-v4zr-fD8">
+              <div class="card-b6n">
+                <div class="auto-group-vyqe-wAe">
+                  <div class="item-4-51x">
+                    <div class="icon-pVL">
+                      <div class="icon-bd-m9g"></div>
+                      <p class="item--6Bx"><img src="./src/assets/box1.svg" alt="" /></p>
+                    </div>
+                    <div class="text-ABp">
+                      <p class="first-click-tests-vAz">First click tests</p>
+                      <p class="while-most-people-en-Sf8">
+                        While most people enjoy casino gambling,
+                      </p>
+                    </div>
+                  </div>
+                  <div class="item-3-ZUr">
+                    <div class="icon-ice"><img src="./src/assets/box3.svg" alt="" /></div>
+                    <div class="text-auk">
+                      <p class="design-surveys-M9p">Design surveys</p>
+                      <p class="sports-betting-lott-V18">
+                        Sports betting, lottery and bingo playing for the fun
+                      </p>
+                    </div>
+                  </div>
+                  <div class="item-2-oXc">
+                    <div class="icon-khk"><img src="./src/assets/box2.svg" alt="" /></div>
+                    <div class="text-bTU">
+                      <p class="preference-tests-9zn">Preference tests</p>
+                      <p class="the-myspace-page-def-gUv">
+                        The Myspace page defines the individual.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="item-1-c7g">
+                  <div class="icon-mFU"><img src="./src/assets/box4.svg" alt="" /></div>
+                  <div class="text-eKG">
+                    <p class="five-second-tests-D7U">Five second tests</p>
+                    <p class="personal-choices-and-XP4">
+                      Personal choices and the overall personality of the
+                      person.{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="button-486">SIGN UP NOW</div>
+            </div>
+          </div>
+        </div>
+        <div class="auto-group-9p26-LUv">
+          <div class="auto-group-hdvy-TJe">
+            <div class="group-200-mq8">
+              <p class="marketing-strategies-5qp">Contents Strategies</p>
+              <p class="join-40000-other-m-QNJ">
+                We focus on ergonomics and meeting you where you work. It&#39;s
+                only a keystroke away.{" "}
+              </p>
+              <div class="auto-group-8gtp-idt">
+                <div class="item-1-4hk">
+                  <img class="image-bg-B1g" src="./src/assets/image-bg.png" />
+                  <p class="by-abdullah-03-mar-UWa">
+                    <span class="by-abdullah-03-mar-UWa-sub-0">By </span>
+                    <span class="by-abdullah-03-mar-UWa-sub-1">Wahid Ari</span>
+                    <span class="by-abdullah-03-mar-UWa-sub-2">
+                      {" "}
+                      | 03 March 2019
+                    </span>
+                  </p>
+                  <p class="increasing-prosperit-vG6">
+                    Increasing Prosperity With Positive Thinking
+                  </p>
+                </div>
+                <div class="item-2-CDc">
+                  <div class="image-8NA">
+                    <img
+                      class="auto-group-kwii-gPg"
+                      src="./src/assets/image-bg-bg.png"
+                    />
+                  </div>
+                  <p class="by-abdullah-03-mar-z9U">
+                    <span class="by-abdullah-03-mar-z9U-sub-0">By </span>
+                    <span class="by-abdullah-03-mar-z9U-sub-1">Wahid Ari</span>
+                    <span class="by-abdullah-03-mar-z9U-sub-2">
+                      {" "}
+                      | 03 March 2019
+                    </span>
+                  </p>
+                  <p class="motivation-is-the-fi-ThG">
+                    Motivation Is The First Step To Success
+                  </p>
+                </div>
+                <div class="item-3-jug">
+                  <div class="image-Gei">
+                    <img
+                      class="auto-group-rt8a-2dt"
+                      src="./src/assets/auto-group-rt8a.png"
+                    />
+                  </div>
+                  <p class="by-abdullah-03-mar-jYJ">
+                    <span class="by-abdullah-03-mar-jYJ-sub-0">By </span>
+                    <span class="by-abdullah-03-mar-jYJ-sub-1">Wahid Ari</span>
+                    <span class="by-abdullah-03-mar-jYJ-sub-2">
+                      {" "}
+                      | 03 March 2019
+                    </span>
+                  </p>
+                  <p class="success-steps-for-yo-RCA">
+                    Success Steps For Your Personal Or Business
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="auto-group-14sg-fEv">
+            <div class="line-BDG"></div>
+            <div class="group-203-iDC">
+              <div class="auto-group-4dje-eMk">
+                <p class="marketing-strategies-NHk">Price Table</p>
+                <p class="marketing-strategies-gpE">
+                  We offer competitive price
+                </p>
+              </div>
+              <div class="auto-group-u8ce-dDg">
+                <div class="frame-50-AzJ">
+                  <p class="increasing-prosperit-UEJ">Free</p>
+                  <p class="increasing-prosperit-NqU">
+                    Brief price description
+                  </p>
+                  <div class="group-201-uaW">
+                    <p class="item-0-2QE">0</p>
+                    <div class="auto-group-7bl2-MBc">
+                      <div class="title-VYi">$</div>
+                      <p class="per-month-nXp">Per / month</p>
+                    </div>
+                  </div>
+                  <div class="group-202-v8E">
+                    <p class="only-2-operators-ScN">Only 2 Operators</p>
+                    <p class="notifications-muY">Notifications</p>
+                    <p class="landing-pages-Hcz">Landing Pages</p>
+                  </div>
+                  <div class="frame-2-1Yz">Order Now</div>
+                </div>
+                <div class="frame-52-Rci">
+                  <p class="increasing-prosperit-Kxz">Standard</p>
+                  <p class="increasing-prosperit-Eq4">
+                    Brief price description
+                  </p>
+                  <div class="group-201-y1x">
+                    <p class="item-5-5an">5</p>
+                    <div class="auto-group-xhr6-CfQ">
+                      <div class="title-vrJ">$</div>
+                      <p class="per-month-Pjt">Per / month</p>
+                    </div>
+                  </div>
+                  <div class="group-202-vE2">
+                    <p class="operators-EVc">5+ Operators</p>
+                    <p class="notifications-xgW">Notifications</p>
+                    <p class="landing-pages-6Gv">Landing Pages</p>
+                  </div>
+                  <div class="frame-2-Ra6">Order Now</div>
+                </div>
+                <div class="frame-51-qNv">
+                  <p class="increasing-prosperit-wRx">Premium</p>
+                  <p class="increasing-prosperit-fsk">
+                    Brief price description
+                  </p>
+                  <div class="group-201-nhU">
+                    <p class="item-10-8mL">10</p>
+                    <div class="auto-group-29rg-4Q6">
+                      <div class="title-oca">$</div>
+                      <p class="per-month-77U">Per / month</p>
+                    </div>
+                  </div>
+                  <div class="group-202-drW">
+                    <p class="operators-ZkA">10+ Operators</p>
+                    <p class="notifications-HgA">Notifications</p>
+                    <p class="landing-pages-Co8">Landing Pages</p>
+                  </div>
+                  <div class="frame-2-vDL">Order Now</div>
+                </div>
+              </div>
+            </div>
+            <p class="figma2html-Z1Q">
+              Макет канала @figma2html
+              <br />
+            </p>
+          </div>
+          <div class="group-206-Dre">
+            <div class="auto-group-mjna-Z9p">
+              <p class="h2-swC">What Clients Say</p>
+              <p class="paragraph-b6W">
+                Problems trying to resolve the conflict between
+                <br />
+                the two major realms of Classical physics: Newtonian mechanics
+              </p>
+            </div>
+            <div class="auto-group-zxla-WjG">
+              <div class="card-item-4Vt">
+                <div class="group-204-mfC">
+                  <img class="vector-Vr6" src="./src/assets/vector-eri.png" />
+                  <img class="vector-1pS" src="./src/assets/vector-qKt.png" />
+                  <img class="vector-wi6" src="./src/assets/vector-eVL.png" />
+                  <img class="vector-gQn" src="./src/assets/vector-4XG.png" />
+                  <img class="vector-DQi" src="./src/assets/vector-3aN.png" />
+                </div>
+                <p class="h6-kvS">
+                  Product helps you see how many
+                  <br />
+                  more days you need to work to
+                  <br />
+                  reach your financial goal.
+                </p>
+                <div class="group-205-h4z">
+                  <div class="ellipse-2-Eai"><img src="./src/assets/image.svg" alt="" /></div>
+                  <div class="auto-group-hm4i-L7x">
+                    <p class="link-fvv">Wahid Ari</p>
+                    <p class="h6-1E6">Designer</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card-item-KEn">
+                <div class="group-204-3Rg">
+                  <src class="vector-Nir" src="./src/assets/vector-LQv.png" />
+                  <img class="vector-i22" src="./src/assets/vector-5mG.png" />
+                  <img class="vector-F1x" src="./src/assets/vector-Pta.png" />
+                  <img class="vector-aK8" src="./src/assets/vector-gNJ.png" />
+                  <img class="vector-iAS" src="./src/assets/vector-KYA.png" />
+                </div>
+                <p class="h6-3yQ">
+                  Product helps you see how many
+                  <br />
+                  more days you need to work to
+                  <br />
+                  reach your financial goal.
+                </p>
+                <div class="group-205-ycA">
+                  <div class="ellipse-2-7iN"><img src="./src/assets/image.svg" alt="" /></div>
+                  <div class="auto-group-macn-Ryx">
+                    <p class="link-n3p">Wahid Ari</p>
+                    <p class="h6-vA2">Designer</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card-item-2Tx">
+                <div class="group-204-kPx">
+                  <img class="vector-Uar" src="./src/assets/vector.png" />
+                  <img class="vector-1an" src="./src/assets/vector-gc6.png" />
+                  <img class="vector-kHU" src="./src/assets/vector-JBx.png" />
+                  <img class="vector-5qY" src="./src/assets/vector-qZ8.png" />
+                  <img class="vector-RuQ" src="./src/assets/vector-xnr.png" />
+                </div>
+                <p class="h6-aXQ">
+                  Product helps you see how many
+                  <br />
+                  more days you need to work to
+                  <br />
+                  reach your financial goal.
+                </p>
+                <div class="group-205-7nE">
+                  <div class="ellipse-2-TLJ"><img src="./src/assets/image.svg" alt="" /></div>
+                  <div class="auto-group-5pqi-Zu8">
+                    <p class="link-K7c">Wahid Ari</p>
+                    <p class="h6-TDp">Designer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="frame-16-Z1x">
+            <div class="auto-group-l4c6-2gE">
+              <p class="join-100-compannies-who-boost-their-business-with-product-9PU">
+                Join 100 Compannies who boost their business with Product
+              </p>
+              <div class="frame-2-pEi">Get This</div>
+            </div>
+            <img class="screen-hJW" src="./src/assets/screen.png" />
+          </div>
+          <p class="figma2html-oMY">
+            Макет канала @figma2html
+            <br />
+          </p>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
